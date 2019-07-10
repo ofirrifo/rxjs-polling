aa88e9b39e27ddc830604d7c1bbc864ceaa87ef2
ofirf <ofirf@sentinel.com>
2019-07-10 17:23:06 +0300

    add demo

--- projects/rxjs-polling-example/src/app/app.module.ts
+++ projects/rxjs-polling-example/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PriceComponent } from './price/price.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PriceComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
