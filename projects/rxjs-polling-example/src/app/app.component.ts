9a487249edf01bb449a185ee0cf428400a4f8e42
ofirf <ofirf@sentinel.com>
2019-07-10 16:02:53 +0300

    add demo

--- projects/rxjs-polling-example/src/app/app.component.ts
+++ projects/rxjs-polling-example/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { pollingOnResolved } from '../../../rxjs-polling/src/lib/rxjs-polling';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rxjs-polling-example';
+  polling$: Observable<any>;
+
+  constructor(private http: HttpClient) {
+    this.startPolling();
+  }
+
+  startPolling(): void {
+    const httpRequest$ = this.http.get(`https://blockchain.info/ticker`);
+    this.polling$ = pollingOnResolved(httpRequest$, 2000).pipe(map((response: { EUR: { last: number } }) => response.EUR.last));
+  }
 }
